e89fe9b34ce58e2dc965adf76468738ac86f6699
inimomon <richiwirjaya@gmail.com>
2024-05-23 06:50:37 +0700

    tambah FirstSection jsx

---
+++ src/page/FirstSection.jsx
@@ -0,0 +1,13 @@
+export default function FirstSection() {
+    return (
+        <div className="h-screen w-full bg-[#DDC5AA]">
+            <div className="pt-36 pb-2 flex justify-center w-full border-b-2 border-gray-800">
+                <h1 className="text-8xl font-extrabold">The Creations</h1>
+            </div>
+            <div className="h-12 w-full flex justify-evenly items-center">
+                <h1 className="font-semibold italic">PHOTOGRAPHY: ROY YOBIHANA</h1>
+                <h1 className="font-semibold italic">A PIECES FROM A PARALLEL REALITY</h1>
+            </div>
+        </div>
+    );
+}
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,3 +1,6 @@
+// Section Import
+import FirstSection from "./FirstSection";
+
 function Home() {
     return (
         <>
@@ -18,17 +21,15 @@ function Home() {
         </div>
         {/* End Main Page */}
 
+
         {/* First Section */}
-        <div className="h-screen w-full bg-[#DDC5AA]">
-            <div className="pt-36 pb-2 flex justify-center w-full border-b-2 border-gray-800">
-                <h1 className="text-8xl font-extrabold">The Creations</h1>
-            </div>
-            <div className="h-12 w-full flex justify-evenly items-center">
-                <h1 className="font-semibold italic">PHOTOGRAPHY: ROY YOBIHANA</h1>
-                <h1 className="font-semibold italic">A PIECES FROM A PARALLEL REALITY</h1>
-            </div>
-        </div>
+            <FirstSection />
         {/* End First Section */}
+
+
+        {/* Footer Section */}
+
+        {/* End Footer Section */}
         </>
     );
 }
